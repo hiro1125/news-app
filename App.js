878f7699bd89ac08285f3e1a704e791ca65ef29f
hiro1125 <hiro112538@gmail.com>
2022-05-29 15:04:51 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: 'https://picsum.photos/id/1003/200/200' }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            体はほっそりとし、四肢が細長く、尾は短い。ふつう雄の頭部には樹枝状の枝角があり、毎年基部から落ちる。森林や草原にすみ、木や草の葉・地衣などを食べる。ニホンジカ・トナカイ・ジャコウジカ・キョン・キバノロ・ノロなどの種類がある。
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,5 +28,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
   },
 });
