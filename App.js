398b9f224f072f693828d83e3f492d671a569cc8
hiro1125 <hiro112538@gmail.com>
2022-05-30 21:54:01 +0900

    View News List

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import NewsArticle from './components/NewsArticle';
+import articles from './dummies/articles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/id/1003/200/200' }}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <NewsArticle
+            ImageUrl={item.urlToImage}
+            Title={item.title}
+            SubTitle={item.author}
           />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            体はほっそりとし、四肢が細長く、尾は短い。ふつう雄の頭部には樹枝状の枝角があり、毎年基部から落ちる。森林や草原にすみ、木や草の葉・地衣などを食べる。ニホンジカ・トナカイ・ジャコウジカ・キョン・キバノロ・ノロなどの種類がある。
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
-      </View>
-    </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -26,29 +25,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
   },
 });
